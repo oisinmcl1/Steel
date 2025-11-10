14003489b798ecbf720636ae24d2f0b1156cf39f
oisinmcl1 <omclaughlin99@gmail.com>
2025-11-10 15:23:39 +0000

    barchart to compare model performance

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Oisin Mc Laughlin
 22441106
 """
 
-from utils import get_data, visualise_data
+from utils import get_data, visualise_data, compare_models
 from knr import default_knr, tuned_knr
 from svr import default_svr, tuned_svr
 
@@ -16,11 +16,22 @@ def main():
     steel_data, scaled_data, x, y, kf = get_data()
     visualise_data(steel_data, scaled_data)
 
-    default_knr(x, y, kf)
-    tuned_knr(x, y, kf)
+    default_knr_train_mse, default_knr_test_mse, default_knr_train_r2, default_knr_test_r2 = default_knr(x, y, kf)
+    tuned_knr_train_mse, tuned_knr_test_mse, tuned_knr_train_r2, tuned_knr_test_r2 = tuned_knr(x, y, kf)
+    default_svr_train_mse, default_svr_test_mse, default_svr_train_r2, default_svr_test_r2 = default_svr(x, y, kf)
+    tuned_svr_train_mse, tuned_svr_test_mse, tuned_svr_train_r2, tuned_svr_test_r2 = tuned_svr(x, y, kf)
 
-    default_svr(x, y, kf)
-    tuned_svr(x, y, kf)
+    compare_models("Default KNR", "Tuned KNR",
+                   default_knr_train_mse, default_knr_test_mse, default_knr_train_r2, default_knr_test_r2,
+                   tuned_knr_train_mse, tuned_knr_test_mse, tuned_knr_train_r2, tuned_knr_test_r2)
+
+    compare_models("Default SVR", "Tuned SVR",
+                   default_svr_train_mse, default_svr_test_mse, default_svr_train_r2, default_svr_test_r2,
+                   tuned_svr_train_mse, tuned_svr_test_mse, tuned_svr_train_r2, tuned_svr_test_r2)
+
+    compare_models("Tuned KNR", "Tuned SVR",
+                   tuned_knr_train_mse, tuned_knr_test_mse, tuned_knr_train_r2, tuned_knr_test_r2,
+                   tuned_svr_train_mse, tuned_svr_test_mse, tuned_svr_train_r2, tuned_svr_test_r2)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ Oisin Mc Laughlin
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 
@@ -48,6 +49,8 @@ def visualise_data(steel_data, scaled_data):
     :param scaled_data: dataframe of scaled steel data
     :return:
     """
+    print("\nVisualising Data")
+
     steel_data.hist(figsize=(12, 10))
     plt.suptitle('Steel Data Before Scaling')
     plt.tight_layout()
@@ -74,5 +77,55 @@ def visualise_data(steel_data, scaled_data):
     plt.figure(figsize=(12, 10))
     plt.title('Correlation Matrix of Steel Data')
     sns.heatmap(data=corr, annot=True)
+    plt.tight_layout()
+    plt.show()
+
+
+def compare_models(model_x_name, model_y_name,
+                   model_x_train_mse, model_x_test_mse, model_x_train_r2, model_x_test_r2,
+                   model_y_train_mse, model_y_test_mse, model_y_train_r2, model_y_test_r2):
+    """
+    Compares two models using a simple bar chart.
+    :param model_x_name: Name of first model
+    :param model_y_name: Name of second model
+    :param model_x_train_mse: Train MSE array for model x
+    :param model_x_test_mse: Test MSE array for model x
+    :param model_x_train_r2: Train R2 array for model x
+    :param model_x_test_r2: Test R2 array for model x
+    :param model_y_train_mse: Train MSE array for model y
+    :param model_y_test_mse: Test MSE array for model y
+    :param model_y_train_r2: Train R2 array for model y
+    :param model_y_test_r2: Test R2 array for model y
+    :return:
+    """
+    print("\nVisualising Comparison between" + model_x_name + "and" + model_y_name)
+
+    labels = ['Train MSE', 'Test MSE', 'Train R2', 'Test R2']
+
+    model_x_scores = [
+        model_x_train_mse.mean(),
+        model_x_test_mse.mean(),
+        model_x_train_r2.mean(),
+        model_x_test_r2.mean()
+    ]
+
+    model_y_scores = [
+        model_y_train_mse.mean(),
+        model_y_test_mse.mean(),
+        model_y_train_r2.mean(),
+        model_y_test_r2.mean()
+    ]
+
+    x = np.arange(4)
+
+    plt.figure(figsize=(12, 10))
+    plt.bar(x=x - 0.2, height=model_x_scores, width=0.4, label=model_x_name)
+    plt.bar(x=x + 0.2, height=model_y_scores, width=0.4, label=model_y_name)
+
+    plt.xticks(x, labels)
+    plt.ylabel('Score')
+    plt.title('Comparison of ' + model_x_name + ' and ' + model_y_name)
+
+    plt.legend()
     plt.tight_layout()
     plt.show()
